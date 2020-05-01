705702449a12f27c79a3960111733270227eb4a0
tamdilip <tamdilip@gmail.com>
2020-05-01 13:18:46 +0530

    Refactored to webpack app

---
+++ index.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './src/css/search.css';
+import './src/css/error.css';
+import './src/css/app.css';
+
+
+import './src/js/app.js';
+import './src/js/search.js';
+
