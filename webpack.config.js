98f405d41430a46130c40b90500aebdb13dc6503
tamdilip <tamdilip@gmail.com>
2020-05-02 13:09:06 +0530

    gh-pages deploy configured

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'demo'),
         filename: '[name].bundle.js',
-        publicPath: '/'
+        publicPath: '/readme-renderer/'
     },
 
     devServer: {
